cc5bf4feedc7d93e806a72b182337b60f9ddefac
Mihai Slobozeanu <mihai.slobozeanu@keez.ro>
2017-09-21 10:03:59 +0300

    1. path can be an url and the swagger schema will be fetched 2. if GQLProxyBaseUrl is not provided, take the baseUrl from swagger schema 3. allow multiple swagger schema and join them using namespaces

--- src/swagger.js
+++ src/swagger.js
@@ -1,39 +1,53 @@
 // @flow
-import type {SwaggerSchema, Endpoint, Responses} from './types';
+import type {SwaggerSchema, Endpoint, Responses } from './types';
+import rp from 'request-promise';
 import refParser from 'json-schema-ref-parser';
-import type {GraphQLParameters} from './types';
+import type {GraphQLParameters } from './types';
 import getRequestOptions from 'node-request-by-swagger';
 let __schema;
 
 export const getSchema = () => {
-  if (!__schema || !Object.keys(__schema).length) {
-    throw new Error('Schema was not loaded');
-  }
-  return __schema;
+	if (!__schema || !Object.keys(__schema).length) {
+		throw new Error('Schema was not loaded');
+	}
+	return __schema;
 };
 
 const getGQLTypeNameFromURL = (method: string, url: string) => {
-  const fromUrl = url.replace(/[\{\}]+/g, '').replace(/[^a-zA-Z0-9_]+/g, '_');
-  return `${method}${fromUrl}`;
+	const fromUrl = url.replace(/[\{\}]+/g, '').replace(/[^a-zA-Z0-9_]+/g, '_');
+	return `${method}${fromUrl}`;
 };
 
 const getSuccessResponse = (responses: Responses) => {
-  let resp;
+	let resp;
 
-  if (!responses) return null;
+	if (!responses) return null;
 
-  Object.keys(responses).some(code => {
-    resp = responses[code];
-    return code[0] === '2';
-  });
+	Object.keys(responses).some(code => {
+		resp = responses[code];
+		return code[0] === '2';
+	});
 
-  return resp && resp.schema;
+	return resp && resp.schema;
 };
 
-export const loadSchema = (pathToSchema: string) => {
-  const schema = refParser.dereference(pathToSchema);
-  __schema = schema;
-  return schema;
+export const loadSchema = async (pathToSchema: string) => {
+	if (typeof pathToSchema === 'string' && pathToSchema.toLowerCase().startsWith('http')) {
+		pathToSchema = await fetchSchema(pathToSchema);
+	}
+	const schema = refParser.dereference(pathToSchema);
+	__schema = schema;
+	return schema;
+};
+
+const fetchSchema = (schemaurl: string) => {
+	var options = {
+		uri: schemaurl,
+		transform: function (body) {
+			return JSON.parse(body);
+		}
+	};
+	return rp(options);
 };
 
 const replaceOddChars = (str) => str.replace(/[^_a-zA-Z0-9]/g, '_');
@@ -41,35 +55,43 @@ const replaceOddChars = (str) => str.replace(/[^_a-zA-Z0-9]/g, '_');
 /**
  * Going throw schema and grab routes
  */
-export const getAllEndPoints = (schema: SwaggerSchema): {[string]: Endpoint} => {
-  const allTypes = {};
-  __schema = schema;
-  Object.keys(schema.paths).forEach(path => {
-    const route = schema.paths[path];
-    Object.keys(route).forEach(method => {
-      const obj = route[method];
-      const isMutation = ['post', 'put', 'patch', 'delete'].indexOf(method) !== -1;
-      const typeName = obj.operationId || getGQLTypeNameFromURL(method, path);
-      const parameters = obj.parameters ? obj.parameters.map(param => {
-        const type = param.type;
-        return {name: replaceOddChars(param.name), type, jsonSchema: param};
-      }) : [];
-      const endpoint: Endpoint = {
-        parameters,
-        description: obj.description,
-        response: getSuccessResponse(obj.responses),
-        request: (args: GraphQLParameters, baseUrl: string) => {
-          const url = `${baseUrl}${path}`;
-          return getRequestOptions(obj, {
-            request: args,
-            url,
-            method: method
-          }, '');
-        },
-        mutation: isMutation
-      };
-      allTypes[typeName] = endpoint;
-    });
-  });
-  return allTypes;
+export const getAllEndPoints = (schema: SwaggerSchema): { [string]: Endpoint } => {
+	const allTypes = {};
+	__schema = schema;
+	var schemaBaseUrl;
+
+	if (schema.schemes && schema.schemes.length && schema.host && schema.basePath) {
+		var url = require('url');
+		const protocol = schema.schemes.indexOf('https') !== -1 ? 'https' : schema.schemes[0];
+		schemaBaseUrl = url.format({ protocol: protocol, host: schema.host, pathname: schema.basePath }).toString();
+	}
+
+	Object.keys(schema.paths).forEach(path => {
+		const route = schema.paths[path];
+		Object.keys(route).forEach(method => {
+			const obj = route[method];
+			const isMutation = ['post', 'put', 'patch', 'delete'].indexOf(method) !== -1;
+			const typeName = obj.operationId || getGQLTypeNameFromURL(method, path);
+			const parameters = obj.parameters ? obj.parameters.map(param => {
+				const type = param.type;
+				return { name: replaceOddChars(param.name), type, jsonSchema: param };
+			}) : [];
+			const endpoint: Endpoint = {
+				parameters,
+				description: obj.description,
+				response: getSuccessResponse(obj.responses),
+				request: (args: GraphQLParameters, baseUrl: string) => {
+					const url = `${baseUrl || schemaBaseUrl}${path}`;
+					return getRequestOptions(obj, {
+						request: args,
+						url,
+						method: method
+					}, '');
+				},
+				mutation: isMutation
+			};
+			allTypes[typeName] = endpoint;
+		});
+	});
+	return allTypes;
 };
--- src/typeMap.js
+++ src/typeMap.js
@@ -1,142 +1,145 @@
 // @flow
-import type {GraphQLType, JSONSchemaType, EndpointParam} from './types';
-import type {GraphQLScalarType} from 'graphql/type/definition.js.flow';
+import type {GraphQLType, JSONSchemaType, EndpointParam } from './types';
+import type {GraphQLScalarType } from 'graphql/type/definition.js.flow';
 import _ from 'lodash';
 import * as graphql from 'graphql';
-import {getSchema} from './swagger';
+import { getSchema } from './swagger';
 
 const __allTypes = {};
 const primitiveTypes = {
-  string: graphql.GraphQLString,
-  date: graphql.GraphQLString,
-  integer: graphql.GraphQLInt,
-  number: graphql.GraphQLInt,
-  boolean: graphql.GraphQLBoolean
+	string: graphql.GraphQLString,
+	date: graphql.GraphQLString,
+	integer: graphql.GraphQLInt,
+	number: graphql.GraphQLInt,
+	boolean: graphql.GraphQLBoolean
 };
 
 const isObjectType = (jsonSchema) =>
-  jsonSchema.properties || jsonSchema.type === 'object' || jsonSchema.type === 'array' || jsonSchema.schema;
+	jsonSchema.properties || jsonSchema.type === 'object' || jsonSchema.type === 'array' || jsonSchema.schema;
 
 const getTypeNameFromRef = (ref: string) => {
-  const cutRef = ref.replace('#/definitions/', '');
-  return cutRef.replace(/\//, '_');
+	const cutRef = ref.replace('#/definitions/', '');
+	return cutRef.replace(/\//, '_');
 };
 
 const getExistingType = (ref: string, isInputType: boolean) => {
-  var typeName = getTypeNameFromRef(ref);
-  const schemaTypeName = typeName;
-  isInputType && (typeName += 'Input');
-  const allSchema = (0, getSchema)();
-  if (!__allTypes[typeName]) {
-    const schema = allSchema.definitions[schemaTypeName];
-    if (!schema) {
-      throw new Error(`Definition ${schemaTypeName} was not found in schema`);
-    }
-    __allTypes[typeName] = createGQLObjectEx(schema, typeName, isInputType);
-  }
-  return __allTypes[typeName];
+	var typeName = getTypeNameFromRef(ref);
+	const schemaTypeName = typeName;
+	const allSchema = (0, getSchema)();
+
+	isInputType && (typeName += 'Input');
+	allSchema.__namespace__ && (typeName = allSchema.__namespace__ + '_' + typeName);
+
+	if (!__allTypes[typeName]) {
+		const schema = allSchema.definitions[schemaTypeName];
+		if (!schema) {
+			throw new Error(`Definition ${schemaTypeName} was not found in schema`);
+		}
+		__allTypes[typeName] = createGQLObjectEx(schema, typeName, isInputType);
+	}
+	return __allTypes[typeName];
 };
 
 const getRefProp = (jsonSchema: JSONSchemaType) => {
-  var typeSchema = jsonSchema.schema || jsonSchema;
-  const allSchema = (0, getSchema)();
-  for(var definitionName in allSchema.definitions){
-    if(allSchema.definitions[definitionName] === typeSchema){
-      return definitionName;
-    }
-  }
-  return jsonSchema.$ref || (jsonSchema.schema && jsonSchema.schema.$ref);
+	var typeSchema = jsonSchema.schema || jsonSchema;
+	const allSchema = (0, getSchema)();
+	for (var definitionName in allSchema.definitions) {
+		if (allSchema.definitions[definitionName] === typeSchema) {
+			return definitionName;
+		}
+	}
+	return jsonSchema.$ref || (jsonSchema.schema && jsonSchema.schema.$ref);
 };
 
 export const createGQLObject = (jsonSchema: JSONSchemaType, title: string, isInputType: boolean): GraphQLType => {
-  if (!jsonSchema) {
-    jsonSchema = { // eslint-disable-line no-param-reassign
-      type: 'object',
-      properties: {},
-      description: '',
-      title: ''
-    };
-  }
+	if (!jsonSchema) {
+		jsonSchema = { // eslint-disable-line no-param-reassign
+			type: 'object',
+			properties: {},
+			description: '',
+			title: ''
+		};
+	}
 
-  const reference = getRefProp(jsonSchema);
+	const reference = getRefProp(jsonSchema);
 
-  if (reference) {
-    return getExistingType(reference, isInputType);
-  }
+	if (reference) {
+		return getExistingType(reference, isInputType);
+	}
 
-  return createGQLObjectEx(jsonSchema, title, isInputType);
+	return createGQLObjectEx(jsonSchema, title, isInputType);
 };
 const createGQLObjectEx = (jsonSchema: JSONSchemaType, title: string, isInputType: boolean): GraphQLType => {
-  if (jsonSchema.type === 'array') {
-    if (isObjectType(jsonSchema.items)) {
-      return new graphql.GraphQLList(createGQLObject(jsonSchema.items, title + '_items', isInputType));
-    }
-    return new graphql.GraphQLList(getPrimitiveTypes(jsonSchema.items));
-  }
+	if (jsonSchema.type === 'array') {
+		if (isObjectType(jsonSchema.items)) {
+			return new graphql.GraphQLList(createGQLObject(jsonSchema.items, title + '_items', isInputType));
+		}
+		return new graphql.GraphQLList(getPrimitiveTypes(jsonSchema.items));
+	}
 
-  title = title ||  jsonSchema.title;  // eslint-disable-line no-param-reassign
-  const description = jsonSchema.description;
-  const fields = getTypeFields(jsonSchema, title, isInputType);
-  if (isInputType) {
-    return new graphql.GraphQLInputObjectType({
-      name: title,
-      description,
-      fields
-    });
-  }
-  return new graphql.GraphQLObjectType({
-    name: title,
-    description,
-    fields
-  });
+	title = title || jsonSchema.title;  // eslint-disable-line no-param-reassign
+	const description = jsonSchema.description;
+	const fields = getTypeFields(jsonSchema, title, isInputType);
+	if (isInputType) {
+		return new graphql.GraphQLInputObjectType({
+			name: title,
+			description,
+			fields
+		});
+	}
+	return new graphql.GraphQLObjectType({
+		name: title,
+		description,
+		fields
+	});
 };
 
 
 export const getTypeFields = (jsonSchema: JSONSchemaType, title: string, isInputType: boolean) => {
-  const fields = _.mapValues(jsonSchema.properties || {}, (propertySchema, propertyName) => {
-    return {
-      description: propertySchema.description,
-      type: jsonSchemaTypeToGraphQL(title, propertySchema, propertyName, isInputType)
-    };
-  });
+	const fields = _.mapValues(jsonSchema.properties || {}, (propertySchema, propertyName) => {
+		return {
+			description: propertySchema.description,
+			type: jsonSchemaTypeToGraphQL(title, propertySchema, propertyName, isInputType)
+		};
+	});
 
-  if (!Object.keys(fields).length) {
-    fields.empty = {
-      description: 'default field',
-      type: graphql.GraphQLString
-    };
-  }
-  return fields;
+	if (!Object.keys(fields).length) {
+		fields.empty = {
+			description: 'default field',
+			type: graphql.GraphQLString
+		};
+	}
+	return fields;
 };
 
 const jsonSchemaTypeToGraphQL = (title: string, jsonSchema: JSONSchemaType, schemaName: string, isInputType: boolean) => {
-  if (isObjectType(jsonSchema)) {
-    return createGQLObject(jsonSchema, title + '_' + schemaName, isInputType);
-  } else if (jsonSchema.type) {
-    return getPrimitiveTypes(jsonSchema);
-  }
-  throw new Error("Don't know how to handle schema " + JSON.stringify(jsonSchema) + ' without type and schema');
+	if (isObjectType(jsonSchema)) {
+		return createGQLObject(jsonSchema, title + '_' + schemaName, isInputType);
+	} else if (jsonSchema.type) {
+		return getPrimitiveTypes(jsonSchema);
+	}
+	throw new Error("Don't know how to handle schema " + JSON.stringify(jsonSchema) + ' without type and schema');
 };
 
 const getPrimitiveTypes = (jsonSchema: JSONSchemaType): GraphQLScalarType => {
-  let jsonType = jsonSchema.type;
-  const format = jsonSchema.format;
-  if (format === 'int64') {
-    jsonType = 'string';
-  }
-  const type = primitiveTypes[jsonType];
-  if (!type) {
-    throw new Error(`Cannot build primitive type "${jsonType}"`);
-  }
-  return type;
+	let jsonType = jsonSchema.type;
+	const format = jsonSchema.format;
+	if (format === 'int64') {
+		jsonType = 'string';
+	}
+	const type = primitiveTypes[jsonType];
+	if (!type) {
+		throw new Error(`Cannot build primitive type "${jsonType}"`);
+	}
+	return type;
 };
 
 export const mapParametersToFields = (parameters: Array<EndpointParam>, typeName: string) => {
-  return parameters.reduce((res, param) => {
-    const type = jsonSchemaTypeToGraphQL('param_' + typeName, param.jsonSchema, param.name, true);
-    res[param.name] = {
-      type
-    };
-    return res;
-  }, {});
+	return parameters.reduce((res, param) => {
+		const type = jsonSchemaTypeToGraphQL('param_' + typeName, param.jsonSchema, param.name, true);
+		res[param.name] = {
+			type
+		};
+		return res;
+	}, {});
 };
