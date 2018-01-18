4fc59ee90c2a6c74c6edb8d7fd37241e724f41a6
Mihai Slobozeanu <mihai.slobozeanu@keez.ro>
2018-01-18 08:08:26 +0200

    1. fix for references. json-schema-ref-parser doesn't use $ref field for references. 2. path can be an url and the swagger schema will be fetched 3. if GQLProxyBaseUrl is not provided, take the baseUrl from swagger schema 4. allow multiple swagger schema and join them using namespaces

--- src/swagger.js
+++ src/swagger.js
@@ -1,5 +1,6 @@
 // @flow
 import type {SwaggerSchema, Endpoint, Responses} from './types';
+import rp from 'request-promise';
 import refParser from 'json-schema-ref-parser';
 import type {GraphQLParameters} from './types';
 import getRequestOptions from 'node-request-by-swagger';
@@ -10,6 +11,10 @@ export const getSchema = () => {
     throw new Error('Schema was not loaded');
   }
   return __schema;
+};
+
+export const setSchema = (schema) => {
+	__schema = schema;
 };
 
 const getGQLTypeNameFromURL = (method: string, url: string) => {
@@ -30,10 +35,23 @@ const getSuccessResponse = (responses: Responses) => {
   return resp && resp.schema;
 };
 
-export const loadSchema = (pathToSchema: string) => {
+export const loadSchema = async  (pathToSchema: string) => {
+	if (typeof pathToSchema === 'string' && pathToSchema.toLowerCase().startsWith('http')) {
+		pathToSchema = await fetchSchema(pathToSchema);
+	}
   const schema = refParser.dereference(pathToSchema);
   __schema = schema;
   return schema;
+};
+
+const fetchSchema = (schemaurl: string) => {
+	var options = {
+		uri: schemaurl,
+		transform: function (body) {
+			return JSON.parse(body);
+		}
+	};
+	return rp(options);
 };
 
 const replaceOddChars = (str) => str.replace(/[^_a-zA-Z0-9]/g, '_');
@@ -43,6 +61,15 @@ const replaceOddChars = (str) => str.replace(/[^_a-zA-Z0-9]/g, '_');
  */
 export const getAllEndPoints = (schema: SwaggerSchema): {[string]: Endpoint} => {
   const allTypes = {};
+	__schema = schema;
+	var schemaBaseUrl;
+
+	if (schema.schemes && schema.schemes.length && schema.host && schema.basePath) {
+		var url = require('url');
+		const protocol = schema.schemes.indexOf('https') !== -1 ? 'https' : schema.schemes[0];
+		schemaBaseUrl = url.format({ protocol: protocol, host: schema.host, pathname: schema.basePath }).toString();
+  }
+  
   Object.keys(schema.paths).forEach(path => {
     const route = schema.paths[path];
     Object.keys(route).forEach(method => {
@@ -58,7 +85,7 @@ export const getAllEndPoints = (schema: SwaggerSchema): {[string]: Endpoint} => 
         description: obj.description,
         response: getSuccessResponse(obj.responses),
         request: (args: GraphQLParameters, baseUrl: string) => {
-          const url = `${baseUrl}${path}`;
+          const url = `${baseUrl || schemaBaseUrl}${path}`;
           return getRequestOptions(obj, {
             request: args,
             url,
