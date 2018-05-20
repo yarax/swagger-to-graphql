0a7752cb95bf2bd1616b455b0f907e4ef1ced74b
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-05-20 18:09:20 +0200

    Add proper integration test

---
+++ test/fixture-test.js
@@ -0,0 +1,24 @@
+const graphQLSchema = require('../lib');
+const graphql = require('graphql');
+const fs = require('fs');
+const expect = require('chai').expect;
+
+describe('Fixture', () => {
+  const directory = `${__dirname}/fixtures/`;
+  fs.readdirSync(directory).forEach(file => {
+    if (file.endsWith('.json')) {
+      describe(file, () => {
+        const graphqlFile = file.replace('.json', '.graphql');
+        it(`should convert to ${graphqlFile}`, () => {
+          return graphQLSchema(directory + file)
+            .then((schema) => {
+              const graphqlfile = directory + graphqlFile;
+              const graphschema = graphql.printSchema(schema);
+              const expected = fs.readFileSync(graphqlfile, 'utf8');
+              expect(graphschema).to.equal(expected);
+            });
+        });
+      })
+    }
+  });
+});
