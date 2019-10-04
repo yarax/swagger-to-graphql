d6102133544a351527a752ef430f1b67be4aec74
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-10-04 14:23:36 +0200

    Simplify node-fetch example

--- example/node-fetch.ts
+++ example/node-fetch.ts
@@ -2,32 +2,44 @@ import fetch from 'node-fetch';
 import { URLSearchParams } from 'url';
 import { CallBackendArguments } from '../src';
 
+function getBodyAndHeaders(
+  body: any,
+  bodyType: 'json' | 'formData',
+  headers: { [key: string]: string } | undefined,
+) {
+  if (!body) {
+    return { headers };
+  }
+
+  if (bodyType === 'json') {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    };
+  }
+
+  return {
+    headers,
+    body: new URLSearchParams(body),
+  };
+}
+
 export async function callBackend({
   requestOptions: { method, body, baseUrl, path, query, headers, bodyType },
 }: CallBackendArguments<{}>) {
   const searchPath = query ? `?${new URLSearchParams(query)}` : '';
   const url = `${baseUrl}${path}${searchPath}`;
+  const bodyAndHeaders = getBodyAndHeaders(body, bodyType, headers);
   const response = await fetch(url, {
     method,
-    ...(body
-      ? {
-          ...(bodyType === 'json' && {
-            headers: {
-              'Content-Type': 'application/json',
-              ...headers,
-            },
-            body: JSON.stringify(body),
-          }),
-          ...(bodyType === 'formData' && {
-            headers,
-            body: new URLSearchParams(body),
-          }),
-        }
-      : { headers }),
+    ...bodyAndHeaders,
   });
 
   const text = await response.text();
-  if (response.status >= 200 && response.status < 300) {
+  if (response.ok) {
     try {
       return JSON.parse(text);
     } catch (e) {
